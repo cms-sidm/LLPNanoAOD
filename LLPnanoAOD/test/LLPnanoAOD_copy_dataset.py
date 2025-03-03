a437034227b0f1ef04ca195219d10043040e44d9
Lovisa <lovisa.rygaard@cern.ch>
2025-03-03 13:14:52 +0100

    copy files configs

---
+++ LLPnanoAOD/test/LLPnanoAOD_copy_dataset.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("COPY")
+
+process.options = cms.untracked.PSet(
+    numberOfThreads = cms.untracked.uint32(4),  # Enable 4 threads
+    numberOfStreams = cms.untracked.uint32(4)   # Use 4 parallel streams
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()  # CRAB will override this with userInputFiles
+)
+
+process.out = cms.OutputModule("NanoAODOutputModule",
+    fileName = cms.untracked.string("dummy.root")  # Won't actually be used
+)
+
+process.endpath = cms.EndPath(process.out)
