a5600eb1929a77cb5dd188558aa5038e576da932
Lovisa <lovisa.rygaard@cern.ch>
2025-03-03 13:13:30 +0100

    including get_pnfs_files function

---
+++ DatasetTools/pnfsFiles/get_pnfs_files.py
@@ -0,0 +1,29 @@
+import os
+
+# Define the paths to the base directories
+base_pnfs_path = '/pnfs/desy.de/cms/tier2'
+
+dataset_dir = '/store/user/lrygaard/ttalps/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/LLPnanoAODv1_LLPminiAOD'
+subdirs = ['240725_165308']
+output_file = 'TTToSemiLeptonic_LLPnanoAODv1_LLPminiAOD_2017.txt'
+
+total_pnfs_files = []
+
+for i in range(20):  # X = 0 to 20
+    for subdir in subdirs:
+        dir = f"{base_pnfs_path}{dataset_dir}/{subdir}/000{i}"
+        print(f"Checking {dir}")
+        if os.path.exists(dir):
+            # list files in the directory
+            files_dir = os.listdir(dir)
+            for file in files_dir:
+                full_path = f'{dataset_dir}/{subdir}/000{i}/{file}'
+                total_pnfs_files.append(full_path)
+
+n_pnfs_files = len(total_pnfs_files)
+
+with open(output_file, 'w') as f:
+    for file_path in total_pnfs_files:
+        f.write(f'{file_path}\n')
+
+print(f"All {n_pnfs_files} pnfs files have been saved to {output_file}.")
